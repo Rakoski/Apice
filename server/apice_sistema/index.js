00c96dd20104ac3a6f2d04ab0bc1e5d3ae31e387
Rakoski <mastrakoski@gmail.com>
2023-11-19 19:34:55 -0300

    dockerizando!
    Finalmente, com tempo de domingo, pude juntar os meus dois repositórios em um só! Vou continuar com ele, e, já dando início as atividades, dockerizei tudo! Agora, desde o front-end até o banco de dados tudo é feito em containers!

---
+++ server/apice_sistema/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const cors = require('cors');
+const Sequelize = require("sequelize");
+
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+    process.env.DB_DBNAME,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+        logging: console.log,
+    }
+);
+
+sequelize.sync({ force: true }).then(() => {
+    console.log('Banco de dados sincronizado.');
+});
+
+const postsRouter = require('./src/routes/router')
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use("/api", postsRouter);
+
+// pra facilitar o netstat
+const porta = 8080;
+
+sequelize.sync()
+    .then(() => {
+        app.listen(porta, () => {
+            console.log("Servidor ta rodando na porta", porta);
+        });
+    })
+    .catch((error) => {
+        console.error("Erro ao sincronizar o sequelize: ", error);
+    });
