50e4ccaf9a4c23763b409bdda06043ad7dcb1d5b
Rakoski <mastrakoski@gmail.com>
2023-12-09 16:55:00 -0300

    Apice-logo adicionada

--- client/src/Autorizacao/Login.js
+++ client/src/Autorizacao/Login.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import "./Autorizacao.css"
+import apiceLogo from "../imagens/apice_logo.png"
 
 const Login = () => {
     const [username, setUsername] = useState('');
@@ -12,30 +13,35 @@ const Login = () => {
     return (
         <div>
         <div className="login-container">
-            <h2>Login</h2>
-            <form>
-                <label className="username-label">
-                    Nome:
-                    <input
-                        type="text"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                </label>
-                <br />
-                <label className="password-label">
-                    Senha:
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </label>
-                <br />
-                <button type="button" onClick={handleLogin}>
-                    Login
-                </button>
-            </form>
+            <img src={apiceLogo} alt={"Logo Ápice sistemas"} style={{ marginTop: "-250px", marginBottom: "100px" }} />
+            <h2 className="login-lable">Login</h2>
+            <section>
+                <form>
+                    <label className="username-label" style={{paddingRight: "40px", margin: "10px"}}>
+                        Nome:
+                        <input
+                            style={{marginLeft: "10px"}}
+                            type="text"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                    </label>
+                    <br />
+                    <label className="password-label" style={{paddingRight: "40px", margin: "10px"}}>
+                        Senha:
+                        <input
+                            style={{marginLeft: "10px"}}
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </label>
+                    <br />
+                    <button type="button" onClick={handleLogin}>
+                        Login
+                    </button>
+                </form>
+            </section>
         </div>
         </div>
     );
